2048b6aaf878dbc88c363161977f701106986237
Raymond Ortiz <raymond.ortiz00@gmail.com>
2019-11-04 14:28:22 -0500

    Added cmd line args to train.py

--- train.py
+++ train.py
@@ -1,12 +1,30 @@
+import argparse
+
 from models.a2c import A2C
 from envs.melee_env import MeleeEnv
 
 
 def main():
-    env = MeleeEnv(render=True)
-    agent = A2C(env)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log', default=False)
+    parser.add_argument('--render', default=False, help='Display Dolphin GUI')
+    parser.add_argument('--self_play', default=False)
+    parser.add_argument('--iso_path', default='../smash.iso', help='Path to MELEE iso')
+    parser.add_argument('--model_path', default='weights/', help='Path to store weights')
+    parser.add_argument('--load_model', default=None, help='Load model from file')
+    parser.add_argument('--num_episdoes', default=10, help='# of games to play')
+    args = parser.parse_args()
 
-    for e in range(10):
+    env = MeleeEnv(log=args.log,
+                   render=args.render,
+                   self_play=args.self_play,
+                   iso_path=args.iso_path)
+    agent = A2C(env, model_path=args.model_path)
+
+    if args.load_model:
+        agent.load_model(args.load_model)
+
+    for e in range(args.num_episodes):
         done = False
         state = env.reset()
 
