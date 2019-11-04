1fcf3f60afa7406d636e587c7c32a01e2b0cfffe
Raymond Ortiz <raymond.ortiz00@gmail.com>
2019-11-03 22:43:07 -0500

    Refactored a2c for semi-continuous output

--- models/a2c.py
+++ models/a2c.py
@@ -7,7 +7,7 @@ import time
 
 from keras.layers import Input, Dense, Flatten
 from keras.models import Model
-from keras.optimizers import RMSprop
+from keras.optimizers import Adam
 from keras.utils import to_categorical
 
 
@@ -16,127 +16,119 @@ class A2C:
                  env,
                  lr=0.0001,
                  gamma=0.99,
-                 epsilon=0.1,
-                 rho=0.99,
+                 cont_acts=6,
+                 disc_acts=12,
                  entropy_reg=0.01,
                  network_width=128
                  model_path='weights/'):
         self.env = env
         self.lr = lr
         self.gamma = gamma
-        self.rms_optimizer = RMSprop(lr=lr, epsilon=epsilon, rho=rho)
+        self.state_shape = env.observation_space.shape
+        self.cont_act_dim = cont_acts
+        self.disc_act_dim = disc_acts
+        self.optimizer = Adam(lr=lr)
 
-        self.shared = self._shared_layers(network_width)
-        self.actor_head = self._actor(network_width)
-        self.critic_head = self._critic(network_width)
-
-        self.actor_opt = self._actor_optimizer(entropy_reg)
-        self.critic_opt = self._critic_optimizer()
+        self.model = self._build_model(network_width)
+        self.cont_opt = self._continuous_optimizer(entropy_reg)
+        self.disc_opt = self._discrete_optimizer(entropy_reg)
+        self.val_opt = self._critic_optimizer()
 
         self.model_path = model_path
 
-    def _shared_layers(self, width):
-        state = Input(shape=self.env.observation_space.shape)
+    def _build_model(self, width):
+        state = Input(shape=self.state_shape)
         layer1 = Flatten()(state)
         layer2 = Dense(width / 2, activation='relu')(layer1)
         layer3 = Dense(width, activation='relu')(layer2)
-        return Model(state, layer3)
 
-    def _actor(self, width):
-        layer4 = Dense(width, activation='relu')(self.shared.output)
-        output = Dense(self.env.action_space.shape[0], activation='softmax')(layer4)
-        return Model(self.shared.input, output)
+        actor = Dense(width, activation='relu')(layer3)
+        mu = Dense(self.cont_act_dim, activation='tanh')(actor)
+        var = Dense(self.cont_act_dim, activation='softplus')(actor)
+        disc = Dense(self.disc_act_dim, activation='sigmoid')(actor)
 
-    def _critic(self, width):
-        layer4 = Dense(width, activation='relu')(self.shared.output)
-        output = Dense(1, activation='linear')(layer4)
-        return Model(self.shared.input, output)
+        critic = Dense(width, activation='relu')(layer3)
+        val = Dense(1, activation='linear')(critic)
 
-    def _actor_optimizer(self, reg):
-        action = K.placeholder(shape=(None, self.env.action_space.shape[0]))
+        return Model(inputs=state, outputs=[mu, var, disc, val])
+
+    def _continuous_optimizer(self, reg):
+        action = K.placeholder(shape=(None, self.cont_act_dim))
         advantages = K.placeholder(shape=(None,))
+        mu, var, _, _ = self.model.output
 
-        weighted_actions = K.sum(action * self.actor_head.output, axis=1)
-        eligibility = K.log(weighted_actions + 1e-10) * K.stop_gradient(advantages)
-        entropy = K.sum(self.actor_head.output
-                * K.log(self.actor_head.output + 1e-10), axis=1)
+        pdf = 1.0 / K.sqrt(2.0 * np.pi * var) * K.exp(-K.square(action - mu) / (2.0 * var))
+        log_pdf = K.log(pdf + K.epsilon())
+        entropy = K.sum(0.5 * (K.log(2.0 * np.pi * var) + 1.0))
+        exp = K.sum(log_pdf * advantages + reg * entropy)
 
-        loss = reg * entropy - K.sum(eligibility)
-        updates = self.rms_optimizer.get_updates(
-                self.actor_head.trainable_weights, [], loss)
+        loss = -exp
+        updates = self.optimizer.get_updates(self.model.trainable_weights, [], loss)
 
         return K.function(
-                [self.actor_head.input, action, advantages], [], updates=updates)
+                [self.model.input, action, advantages], [], updates=updates)
+
+    def _discrete_optimizer(self, reg):
+        action = K.placeholder(shape=(None, self.disc_act_dim))
+        advantages = K.placeholder(shape=(None,))
+        _, _, disc, _ = self.model.output
+
+        weighted_actions = K.sum(action * disc, axis=1)
+        eligibility = K.log(weighted_actions + 1e-10) * K.stop_gradient(advantages)
+        entropy = K.sum(disc * K.log(disc + 1e-10), axis=1)
+
+        loss = reg * entropy - K.sum(eligibility)
+        updates = self.optimizer.get_updates(self.model.trainable_weights, [], loss)
+
+        return K.function(
+                [self.model.input, action, advantages], [], updates=updates)
 
     def _critic_optimizer(self):
         discounted_r = K.placeholder(shape=(None,))
+        _, _, _, val = self.model.output
 
-        loss = K.mean(K.square(discounted_r - self.critic_head.output))
-        updates = self.rms_optimizer.get_updates(
-                self.critic_head.trainable_weights, [], loss)
+        loss = K.mean(K.square(discounted_r - val))
+        updates = self.optimizer.get_updates(self.model.trainable_weights, [], loss)
 
         return K.function(
-                [self.critic_head.input, self.discounted_r], [], updates=updates)
+                [self.model.input, discounted_r], [], updates=updates)
 
     def act(self, state):
-        return np.random.choice(np.arange(self.env.action_space.shape[0]),
-                                1,
-                                p=self.actor_head.predict(state).ravel())[0]
+        mu, var, disc, _ = self.model.predict(state)
 
-    def _discount(self, rewards):
-        cumul_r = 0
-        discounted_r = np.zeros_like(rewards)
+        epsilon = np.random.randn(self.cont_act_dim)
+        cont_act = mu + np.sqrt(var) * epsilon
+        cont_act = np.clip(cont_act, 0, 255)
 
-        for t in range(len(rewards) - 1, -1, -1):
-            cumul_r = rewards[t] + cumul_r * self.gamma
-            discounted_r[t] = cumul_r
+        disc_act = np.random.random(self.disc_act_dim) < disc
+        return np.concatenate([cont_act, disc_act])
 
-        return discounted_r
+    def train(self, state, action, reward, next_state, done):
+        cont_act = action[:self.cont_act_dim]
+        disc_act = action[self.cont_act_dim:]
 
-    def _train_models(self, states, actions, rewards, done):
-        discounted_rewards = self._discount(rewards)
-        state_values = self.critic_head.predict(np.array(states))
-        advantages = discounted_rewards - np.reshape(state_values, len(state_values))
-        self.actor_opt([states, actions, advantages])
-        self.critic_opt([states, discounted_rewards])
+        _, _, _, val = self.model.predict(state)
+        target = reward
 
-    def train(self, episodes, summary_writer):
-        for e in range(episodes):
-            time, cumul_reward, done = 0, 0, False
-            old_state = self.env.reset()
-            actions, states, rewards = [], [], []
+        if not done:
+            _, _, _, next_val = self.model.predict(next_state)
+            target += self.gamma * next_val
+        
+        advantage = target - val
+        self.cont_opt([state, cont_act, advantage])
+        self.disc_opt([state, disc_act, advantage])
+        self.val_opt([state, target])
 
-            while not done:
-                action = self.act(old_state)
-                new_state, reward, done, _ = self.env.step(action)
-                actions.append(to_categorical(action, self.env.action_space.shape[0]))
-                rewards.append(reward)
-                states.append(old_state)
-                old_state = new_state
-                cumul_reward += reward
-                time += 1
-            
-            self._train_models(states, actions, rewards, done)
-            self.save_models()
-
-            score = tf.Summary(value=[
-                tf.Summary.Value(tag='score', simple_value=cumul_reward)])
-            summary_writer.add_summary(score, global_step=e)
-            summary_writer.flush()
-
-    def save_models(self):
+    def save_model(self):
         if not os.path.exists(self.model_path):
             os.makedirs(self.model_path)
 
         timestamp = time.asctime().replace(' ', '_')
-        self.actor_head.save_weights(
-                os.path.join(self.model_path, timestamp + '_actor.h5'))
-        self.critic_head.save_weights(
-                os.path.join(self.model_path, timestamp + '_critic.h5'))
+        self.model.save_weights(
+                os.path.join(self.model_path, timestamp + '_model.h5'))
 
-    def load_models(self, actor_path, critic_path):
-        if not os.path.exists(actor_path) or not os.path.exists(critic_path):
+    def load_model(self, path):
+        if not os.path.exists(path):
             return
 
-        self.actor_head.load_weights(actor_path)
-        self.critic_head.load_weights(critic_path)
+        self.model.load_weights(path)
