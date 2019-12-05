2f1ebd09e6add6c96d4dfa98403951b5577a3520
lealex262 <lealex262@gmail.com>
2019-12-05 17:13:43 -0500

    Added state preprocessing step

--- dataset.py
+++ dataset.py
@@ -4,6 +4,8 @@ import numpy as np
 import argparse
 import os
 
+from melee import enums
+
 
 def get_data_from_logs(file_dir, both=True):
     file_list = os.listdir(file_dir)
@@ -28,8 +30,10 @@ def get_data_from_logs(file_dir, both=True):
                 states.append(state)
                 actions.append(action[:, :num_actions])
 
+    # Preprocess
+    states = preprocess_states(np.vstack(states))
 
-    return np.vstack(states), np.vstack(actions)
+    return states, np.vstack(actions)
 
 
 def read_data(file_path, num_states=31, num_actions=32):
@@ -37,3 +41,26 @@ def read_data(file_path, num_states=31, num_actions=32):
     states = data[:, 1:num_states + 1]
     actions = data[:, -num_actions:]
     return states, actions
+
+
+def preprocess_states(states):
+    # Convert Actions to One Hot Encodeing
+    num_actions = 233
+    action2idx_mapping = dict()
+
+    # Convert Value to Indexes
+    for action in enumerate(enums.Action):
+        action2idx_mapping[action[1].value] = int(action[0])
+    
+    for ii in range(states.shape[0]):
+        states[ii][7] = action2idx_mapping[states[ii][7]]
+        states[ii][22] = action2idx_mapping[states[ii][22]]
+
+    # Convert to One Hot Encodeing
+    p1_actions = np.eye(num_actions)[states[:,7].astype(int)]
+    p2_actions = np.eye(num_actions)[states[:,22].astype(int)]
+
+    # Merge
+    new_states = np.hstack([states[:, :7], p1_actions, states[:, 8:22], p2_actions, states[:, 23:]])
+
+    return new_states
