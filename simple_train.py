2226ebb0103a9d88c5b3b73e1181581064b1d928
Sahit Chintalapudi <chsahit@gmail.com>
2019-12-10 05:20:38 -0500

    train  from data

--- simple_train.py
+++ simple_train.py
@@ -105,19 +105,76 @@ def _copy_pasted_train_func():
 
 def main():
     model = ActorCriticPolicy(495, 345, 200)
-    ac_optimizer = optim.Adam(actor_critic.parameters(), lr=learning_rate)
-    do_warm_start(model, optimizer)
+    ac_optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    mean_losses, mean_actors, mean_critics = do_warm_start(model, ac_optimizer)
+    fig, ax = plt.subplots(2)
+    ax[0].plot(mean_actors)
+    ax[1].plot(mean_critics)
+    plt.show()
 
 def do_warm_start(model, optimizer):
     states, actions, action_set = get_data_from_logs("/home/sc/school/cs8803/libmelee/logs/", one_hot_actions = True)
-    '''
-    next_states = np.roll(states, -1, axis = 0)
-    p1_scores = (1000 * (next_states[:,5] - state[:, 5]) -
-            next_states[:,4] - state[:, 4])
+    mean_losses = list()
+    mean_actors, mean_critics = list(), list()
+    for i in range(states.shape[0]):
+        if i % 1000 == 0:
+            print(i)
+            val_loss, val_act, val_crit = validate(model, states, actions)
+            mean_losses.append(val_loss)
+            mean_actors.append(val_act)
+            mean_critics.append(val_crit)
+        state = states[i]
+        if state[5] == 0 or state[20] == 0:
+                continue
+        action = actions[i]
+        next_state = states[i + 1]
+        done = False
+        p1_score = (1000 * (next_state[5] - state[5]) -
+                    (next_state[4] - state[4]))
+        p2_score = (1000 * (next_state[20] - state[20]) -
+                    (next_state[19] - state[19]))
+        if next_state[5] == 0 or next_state[20] == 0:
+            done = True
+        reward = p1_score - p2_score
+
+        value, policy_dist = model.forward(state)
+        dist = policy_dist.detach().numpy()
+        #value = value.detach().numpy()[0, 0]
+
+        # TODO is this correct?
+        Q = torch.Tensor([[reward]])
+        if not done:
+            next_value, _ = model.forward(next_state)
+            next_value = next_value.detach().numpy()[0, 0]
+            Q += gamma * next_value
+        #advantage = torch.FloatTensor([Q - value])
+        advantage = (Q - value).squeeze(0)
+        true_action = np.where(action == 1)[0]
+        log_prob = torch.log(policy_dist.squeeze(0)[true_action])
+        actor_loss = -log_prob * advantage
+        critic_loss = 0.5 * torch.pow(advantage, 2)
+        ac_loss = actor_loss + critic_loss
+
+        optimizer.zero_grad()
+        ac_loss.backward()
+        optimizer.step()
+    return mean_losses, mean_actors, mean_critics
+
+def validate(model, states, actions):
+    '''next_states = np.roll(states, -1, axis = 0)
+    p1_scores = (1000 * (next_states[:,5] - states[:, 5]) -
+        next_states[:,4] - states[:, 4])
     p2_scores = (1000 * (next_states[:,20] - states[:,20]) -
-            next_states[:,19] - states[:,19])
+        next_states[:,19] - states[:,19])
     rewards = p1_scores - p2_scores
-    '''
+    values, policy_dists = model.forward(states)
+    next_values = model.forward(next_states)
+    dones = not (next_state[:,5] == 0 or next_states[:,20] == 0)
+    dones = dones.float()
+    Q = reward + dones * next_values
+    advantages = Q - values'''
+    ac_losses = list()
+    actor_losses, critic_losses = list(), list()
     for i in range(states.shape[0]):
         state = states[i]
         if state[5] == 0 or state[20] == 0:
@@ -143,16 +200,19 @@ def do_warm_start(model, optimizer):
             next_value, _ = model.forward(next_state)
             next_value = next_value.detach().numpy()[0, 0]
             Q += gamma * next_value
-        advantage = Q - value
+        advantage = torch.FloatTensor([Q - value])
         true_action = np.where(action == 1)[0]
         log_prob = torch.log(policy_dist.squeeze(0)[true_action])
         actor_loss = -log_prob * advantage
-        critic_loss = 0.5 * advantage.pow(2)
+        critic_loss = 0.5 * torch.pow(advantage, 2)
         ac_loss = actor_loss + critic_loss
-
-        optimizer.zero_grad()
-        ac_loss.backward()
-        optimizer.step()
+        actor_losses.append(actor_loss.detach().numpy()[0])
+        critic_losses.append(critic_loss.detach().numpy()[0])
+        ac_losses.append(ac_loss.detach().numpy()[0])
+    mean_loss = sum(ac_losses)/len(ac_losses)
+    mean_actor = sum(actor_losses)/len(actor_losses)
+    mean_critic = sum(critic_losses)/len(critic_losses)
+    return  mean_loss, mean_actor, mean_critic
 
 if __name__ == "__main__":
     main()
